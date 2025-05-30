ce43181773ab905fa68e0fcce910f88bfa94a8ba
ismael-2306347 <163002396+ismael-2306347@users.noreply.github.com>
2025-05-30 17:14:55 -0300

    Update actividad_domain.go

--- backend/domain/actividad_domain.go
+++ backend/domain/actividad_domain.go
@@ -11,3 +11,15 @@ type Activity struct {
 	HoraInicio  string `json:"hora_inicio"`                 // Ej: "08:00", "10:30"
 	HoraFin     string `json:"hora_fin"`                    // Ej: "09:00", "11:30"
 }
+
+type ActivityResponse struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Profesor    string `json:"profesor"`
+	Categoria   string `json:"categoria"`
+	Cupos       int    `json:"cupos"`
+	Description string `json:"description"`
+	Dia         int    `json:"dia"`
+	HoraInicio  string `json:"hora_inicio"`
+	HoraFin     string `json:"hora_fin"`
+}
