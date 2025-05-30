6ac10ec95fb6a87ac29489a92f2368e77523fb63
ismael-2306347 <163002396+ismael-2306347@users.noreply.github.com>
2025-05-30 17:15:29 -0300

    Update inscripcion_domain.go

--- backend/domain/inscripcion_domain.go
+++ backend/domain/inscripcion_domain.go
@@ -6,8 +6,22 @@ type Inscripcion struct {
 	Usuario   User `gorm:"foreignkey:UsuarioId"`
 	UsuarioId int
 
-	ActDeportiva Activity `gorm:"foreignkey:ActividadId"`
-	ActividadId  int
+	Actividad   Activity `gorm:"foreignkey:ActividadId"`
+	ActividadId int
 }
 
 type Inscripciones []Inscripcion
+
+type InscripcionRequest struct {
+	UsuarioId   int `json:"usuario_id" `
+	ActividadId int `json:"actividad_id"`
+}
+
+// InscripcionResponse representa la estructura de respuesta para una inscripción
+type InscripcionResponse struct {
+	Id          int              `json:"id"`
+	UsuarioId   int              `json:"usuario_id"`
+	ActividadId int              `json:"actividad_id"`
+	Usuario     UserResponse     `json:"usuario"`
+	Actividad   ActivityResponse `json:"actividad"`
+}
