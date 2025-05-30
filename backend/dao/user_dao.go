aaccfc1afa16e53f34e816aba561c55f1dd7cd77
ismael-2306347 <163002396+ismael-2306347@users.noreply.github.com>
2025-05-30 17:14:06 -0300

    Update user_dao.go

--- backend/dao/user_dao.go
+++ backend/dao/user_dao.go
@@ -2,6 +2,7 @@ package dao
 
 type User struct {
 	ID           int    `gorm:"primary_key"`
+	Name         string `gorm:"not_null"`
 	Username     string `gorm:"unique"`
 	PasswordHash string `gorm:"not_null"`
 	IsAdmin      bool   `gorm:"default:false"`
