7ddbd4b9049d3193af729f6e4631d2696242d95f
ismael-2306347 <163002396+ismael-2306347@users.noreply.github.com>
2025-05-30 17:16:16 -0300

    Update usuario_domain.go

--- backend/domain/usuario_domain.go
+++ backend/domain/usuario_domain.go
@@ -2,7 +2,14 @@ package domain
 
 type User struct {
 	ID       int    `json:"id"`
+	Name     string `json:"name"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	IsAdmin  bool   `json:"is_admin"`
+}
+
+type UserResponse struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
 	IsAdmin  bool   `json:"is_admin"`
 }
